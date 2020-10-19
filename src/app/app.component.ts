86c44ef6efd10a50b69b00c3f085e0847aaf6d7a
jcarvallo <jcarvallo.ar@gmail.com>
2020-10-19 02:13:38 -0300

    add mock avenidas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MockAvenidasService } from '../app/services/mock-avenidas.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MockAvenidasService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private mockAvenidas: MockAvenidasService) {}
   title = 'avenidas';
+
+  ngOnInit() {
+    try {
+      this.mockAvenidas.getAvenidas().subscribe(
+        (res) => console.log(res),
+        (error) => console.log(error)
+      );
+    } catch (error) {}
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {InMemoryDataAvenidasService} from '../app/services/in-memory-data-avenidas.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataAvenidasService),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
