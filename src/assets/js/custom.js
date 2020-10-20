5a72718f24ff6fd88200bf21605c26f0b2433094
jcarvallo <jcarvallo.ar@gmail.com>
2020-10-19 21:17:15 -0300

    update custom.js

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,17 +1,17 @@
 
-// function header stiky
-window.scrollBy(100, 100);
-window.addEventListener("scroll", (e) => {
-  
-  if (window.pageYOffset > 200) {
-    alert('test')
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+window.addEventListener("scroll",function(e) {
+   let header = document.getElementById("myHeader");
+   let scrollPosition =
+     window.pageYOffset ||
+     document.documentElement.scrollTop ||
+     document.body.scrollTop ||
+     0;
+   if (scrollPosition > 200) {
+     header.classList.add("sticky");
+   } else {
+     header.classList.remove("sticky");
+   }
 }, true)
 
-  var header = document.getElementById("myHeader");
- 
 
-// function header stiky
+
