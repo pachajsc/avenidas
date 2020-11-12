b8cc44099eab5cba698e8e74cd95c978a77bb2c9
jose <pachajsc@gmail.com>
2020-11-12 12:39:10 -0300

    ultimos cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   avenueURL:
-    'http://40.124.121.241:8001/avenues/lists?id=5fa18fa9b177bc001841793c',
+    'http://40.124.121.241:8001/avenues/lists',
 };
 
 /*
